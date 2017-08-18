55dc5e59093cb8c4824d88004ee4b4a6a8b9aa3c
juyerina <j35148@gmail.com>
2017-08-18 13:32:27 +0900

    [reactjs] summary JSX

---
+++ JS-study/day2/es6.js
@@ -0,0 +1,13 @@
+require("babel-core").transform("code", options);
+
+let snack = "honey";
+
+function getFood(food){
+  if (food){
+    let snack = "swing";
+    
+    return snack;
+  }
+  return snack;
+}
+
